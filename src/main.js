49594cf28228acb64316468884f1f1680d298fce
DXnima <45487731+DXwangnima@users.noreply.github.com>
2020-03-25 20:27:32 +0800

    左侧导航栏实现

--- src/main.js
+++ src/main.js
@@ -10,6 +10,11 @@ import './assets/css/global.css'
 import axios from 'axios'
 //配置请求的根路径
 axios.defaults.baseURL='http://localhost:8888/api/private/v1/'
+// 请求拦截 请求头添加token
+axios.interceptors.request.use(config=>{
+  config.headers.Authorization=window.sessionStorage.getItem("token")
+  return config
+})
 Vue.prototype.$http=axios
 
 Vue.config.productionTip = false
