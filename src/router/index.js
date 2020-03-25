5331dcf3cf07ce4683b2acae692c5a07e15f2e95
DXnima <45487731+DXwangnima@users.noreply.github.com>
2020-03-25 17:41:43 +0800

    登陆完成
    挂载路由导航 拦截页面跳转到登陆页面
    退出登陆等

--- src/router/index.js
+++ src/router/index.js
@@ -15,4 +15,21 @@ const router = new VueRouter({
   routes
 })
 
+
+//为路由对象，添加beforeEach导航守卫
+//to 将要访问路径
+//from 从哪个路径跳转而来
+//next 函数 表示方向
+router.beforeEach((to, from, next) => {
+    //如果用户访问的登录页，直接放行
+    if (to.path === '/login') return next()
+    //从sessionStorage 中获取到保存的token值
+    const tokenStr = window.sessionStorage.getItem( 'token')
+    //没有token,强制跳转到登录页
+    if (!tokenStr)  return next('/login')
+    next()
+  }
+);
+
+
 export default router
